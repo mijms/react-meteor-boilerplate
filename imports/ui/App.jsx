b01236fc3156f57c6be6bc8c9f540e4834b4f9a1
Mohamed Gouda <gouda@localhost.localdomain>
2017-07-01 18:16:51 +0200

    react meteor boilerplate

---
+++ imports/ui/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>it works</h1>
+        </header>
+      </div>
+    );
+  }
+}
